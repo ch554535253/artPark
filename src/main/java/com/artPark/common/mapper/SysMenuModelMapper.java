77a0484ea7194ac56c83fc8566f0199ffe9d040d
ch554535253 <554535253@qq.com>
2020-11-13 16:25:40 +0800

    mybatis generator update

--- src/main/java/com/artPark/common/mapper/SysMenuModelMapper.java
+++
@@ -1,7 +0,0 @@
-package com.artPark.common.mapper;
-
-import com.artPark.common.model.SysMenuModel;
-import com.artPark.common.plugin.BasicMapper;
-
-public interface SysMenuModelMapper extends BasicMapper<SysMenuModel> {
-}
