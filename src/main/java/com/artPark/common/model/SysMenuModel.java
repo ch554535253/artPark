146105deb82a064e68820613a42d971333eafe60
ch554535253 <554535253@qq.com>
2020-11-13 16:57:56 +0800

    mybatis generator update

---
+++ src/main/java/com/artPark/common/model/SysMenuModel.java
@@ -0,0 +1,55 @@
+package com.artPark.common.model;
+
+import com.artPark.common.plugin.BasicModel;
+
+public class SysMenuModel extends BasicModel {
+    private Integer menuId;
+
+    private String menuName;
+
+    private String menuUrl;
+
+    private Integer priority;
+
+    private String status;
+
+    public Integer getMenuId() {
+        return menuId;
+    }
+
+    public void setMenuId(Integer menuId) {
+        this.menuId = menuId;
+    }
+
+    public String getMenuName() {
+        return menuName;
+    }
+
+    public void setMenuName(String menuName) {
+        this.menuName = menuName;
+    }
+
+    public String getMenuUrl() {
+        return menuUrl;
+    }
+
+    public void setMenuUrl(String menuUrl) {
+        this.menuUrl = menuUrl;
+    }
+
+    public Integer getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Integer priority) {
+        this.priority = priority;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
